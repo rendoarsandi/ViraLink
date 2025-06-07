f0c73f20f90b0198fa0c06f9c4f0c0d6739420bf
rendoarsandi <rendoarsandi@gmail.com>
2025-06-07 16:14:28 +0000

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import { Inter } from "next/font/google"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
